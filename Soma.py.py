d698aa0e6fa8c37be8d1b17a328b3e2f7c744dcd
Hug0Fer <hugolfernandes9@gmail.com>
2024-12-16 10:23:24 +0000

    Criado soma.py

---
+++ Soma.py.py
@@ -0,0 +1,17 @@
+'''
+   Programador...: (C) 2024 Hugo Fernandes
+   Data..........: 13/12/2024
+   Observações...: Soma dois valores
+'''
+
+a = 0
+b = 0
+soma = 0
+print(type(a))
+a = int(input("Introduza um número inteiro: "))
+print(type(b))
+b = int(input("Introduza um número inteiro: "))
+
+soma = a + b
+
+print("O resultado da soma", a," + ", b, " = ", soma)
